e78bf915f891f67722378edd7702ab60ebc9ecf6
Sina <sinarashidiazar@hotmail.com>
2021-04-15 17:16:08 +0430

    Version 0.0.2

--- bk.sh
+++ bk.sh
@@ -3,6 +3,14 @@ DIR=/backup/script/
 DAY=$(date +%u) # 1 is Monday [1:7]
 DAY_STATUS=""
 
+function delete_old_backup(){
+	cd $DIR$DAY_STATUS
+	pwd
+	find $DIR$DAY_STATUS -mindepth 1 -mtime +5 -delete
+	mysqldump -u DBUSERNMAE -pPASSWORDDB DBNAME  > $DIR$DAY_STATUS/DB_BK_`date +%Y%m%d%H%M`_.sql
+}
+
+
 if [[ -d $DIR"Sunday" && -d $DIR"Monday" &&  $DIR"Tuesday" && $DIR"Wednesday" && $DIR"Thursday" && $DIR"Friday" && $DIR"Saturday"  ]];
 then
 	
@@ -14,35 +22,37 @@ then
 	1)
 	 echo "Monday"
 	 DAY_STATUS="Monday"
+	 delete_old_backup
 	 ;;
 	2)
 	 echo "Tuesday"
 	 DAY_STATUS="Tuesday"
-	 cd $DIR$DAY_STATUS
-	 FILE=$(ls -t1 |  head -n 1 | awk -F'.' '{print $1}') 
-	 if test -z "$FILE";then
-		 echo "empty"
-	 fi
+	 delete_old_backup
 	 ;;
  	3)
 	 echo "Wednesday"
 	 DAY_STATUS="Wednesday"
+	 delete_old_backup
 	 ;;
  	4)
 	 echo "Thursday"
 	 DAY_STATUS="Thursday"
+	 delete_old_backup 
 	 ;;
  	5)
 	 echo "Friday"
 	 DAY_STATUS="Friday"
+	 delete_old_backup
 	 ;;
 	6)
 	 echo "Saturday"
 	 DAY_STATUS="Saturday"
+	 delete_old_backup
 	 ;;
 	7)
 	 echo "Sunday"
 	 DAY_STATUS="Sunday"
+	 delete_old_backup
 	 ;;
 	*)
 	 echo "Error Date."
